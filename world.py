aa495b2d43b3d6e2f8cae322820a154f7e382208
Administrator <admin@local.host>
2015-04-09 15:46:24 -0700

    Added the world

---
+++ world.py
@@ -0,0 +1,21 @@
+
+class GameNode:
+  """Represents a position in the game world
+  """
+
+  def __init__(text=None, north=None, south=None, east=None, west=None):
+    """
+    Args:
+      text: str - Text that is displayed to player upon entering position
+      north: GameNode - Pointer to the position north
+      south: GameNode - Pointer to the position south
+      east: GameNode - Pointer to the position east
+      west: GameNode - Pointer to the position west
+    """
+    self.text = text
+    self.north = north
+    self.south = south
+    self.east = east
+    self.west = west
+
+
